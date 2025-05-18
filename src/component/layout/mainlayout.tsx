c0a88a76a51a304a4107f442f98f84312686b18c
Shikhar Khadka <shikhar@cosys.com.np>
2025-05-18 18:00:39 +0545

    layout updated

--- src/component/layout/mainlayout.tsx
+++ src/component/layout/mainlayout.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { Outlet } from "react-router";
 
 export const Mainlayout = () => {
-  return <div>Mainlayout</div>;
+  return (
+    <div>
+      <div>This is header</div>
+      <Outlet />
+      <div>This is footer</div>
+    </div>
+  );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import { Provider } from "react-redux";
 import { store } from "./store/store.js";
 import { Category } from "./pages/category/category.js";
 import { AddBlog } from "./pages/blog/addblog.js";
+import { Mainlayout } from "./component/layout/mainlayout.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -15,9 +16,11 @@ createRoot(document.getElementById("root")).render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />}></Route>
-          <Route path="/blog" element={<Blog />}></Route>
-          <Route path="/category" element={<Category />}></Route>
-          <Route path="/addBlog" element={<AddBlog />}></Route>
+          <Route element={<Mainlayout />}>
+            <Route path="/blog" element={<Blog />}></Route>
+            <Route path="/category" element={<Category />}></Route>
+            <Route path="/addBlog" element={<AddBlog />}></Route>
+          </Route>
         </Routes>
       </BrowserRouter>
     </Provider>
